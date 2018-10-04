75461b76a55e9ba6ec3188ce93178bab4705b575
Ægir Æxx <aegir.tomasson@gmail.com>
2018-10-04 18:59:11 +0000

    Changed back the offers to hold the index of the candy, instead of the candy itself

--- data/collections.js
+++ data/collections.js
@@ -31,39 +31,12 @@ const offers = [
     {
         id: 1,
         name: 'Two unique',
-        candies: [
-            {
-                id: 1,
-                name: 'Snowkers',
-                description: 'Similar to Snickers but white as the snow.'
-            },
-            {
-                id: 2,
-                name: 'Venus',
-                description: 'Who wants Mars, when you can get Venus!'
-            }
-        ]
+        candies: [ 1, 2 ]
     },
     {
         id: 2,
         name: 'The Last of Us',
-        candies: [
-            {
-                id: 3,
-                name: 'Brix',
-                description: 'Just like Twix, but shaped like a brick.'
-            },
-            {
-                id: 4,
-                name: 'Rootsie Toll',
-                description: 'An upside down Tootsie Roll!'
-            },
-            {
-                id: 5,
-                name: 'Z&Zs',
-                description: 'M&M has made itself a powerful enemy.. We call it Zett-N-Zetts!'
-            }
-        ]
+        candies: [ 3, 4, 5 ]
     }
 ];
 
